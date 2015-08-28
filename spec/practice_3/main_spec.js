590342889ec05b40a447720a109078066df6937c
jtong <tj19832@gmail.com>
2015-08-28 22:21:39 +0800

    jtong - finish practice 3

--- spec/practice_3/main_spec.js
+++ spec/practice_3/main_spec.js
@@ -5,11 +5,42 @@ var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
+var Person = require("../../src/practice_3/person.js");
+var Student = require("../../src/practice_3/student.js");
 
-
-describe("OO入门", function() {
-    it("覆盖父类的方法-1", function () {
-
+describe("Person", function(){
+    it("should have field name and age", function(){
+        var person = new Person("Tom", 21);
+        expect(person.name).to.equal("Tom");
+        expect(person.age).to.equal(21);
 
     });
+
+    it("should have a method introduce, introduce person with name and age", function(){
+        var person = new Person("Tom", 21);
+
+        var introduce = person.introduce();
+
+        expect(introduce).to.equal("My name is Tom. I am 21 years old.");
+
+    });
+
+    describe("Student", function(){
+        it("should have field name, age and class number", function(){
+            var student = new Student("Tom", 21, 2);
+            expect(student.name).to.equal("Tom");
+            expect(student.age).to.equal(21);
+            expect(student.klass).to.equal(2);
+
+        });
+
+        it("should overwrite Person introduce, introduce student with class", function(){
+            var student = new Student("Tom", 21, 2);
+            var introduce = student.introduce();
+
+            expect(introduce).to.equal("I am a Student. I am at Class 2.");
+
+        });
+
+    })
 });
