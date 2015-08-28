faa4d68ad4e5a629b938b74936420d9053f1bcc0
jtong <tj19832@gmail.com>
2015-08-28 21:33:05 +0800

    init and add step 1

---
+++ spec/practice_1/main_spec.js
@@ -0,0 +1,26 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+var Person = require("../../src/practice_1/person.js");
+
+describe("Person", function(){
+    it("should have field name and age", function(){
+        var person = new Person("Tom", 21);
+        expect(person.name).to.equal("Tom");
+        expect(person.age).to.equal(21);
+
+    });
+    
+    it("should have a method introduce, introduce person with name and age", function(){
+        var person = new Person("Tom", 21);
+        
+        var introduce = person.introduce();
+        
+        expect(introduce).to.equal("My name is Tom. I am 21 years old.");
+        
+    });
+});
---
+++ spec/practice_6/main_spec.js
@@ -0,0 +1,15 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+
+
+describe("OO入门", function() {
+    it("覆盖同时复用父类被覆盖的方法", function () {
+
+    });
+});
